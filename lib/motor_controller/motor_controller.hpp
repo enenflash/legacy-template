833f425bbf52d8563277a5fa6eb6c02915d134fc
enenflash <hwee.en.tang@gmail.com>
2025-05-10 17:30:14 +0800

    cut out unnecessary setup function

--- lib/motor_controller/motor_controller.hpp
+++ lib/motor_controller/motor_controller.hpp
@@ -31,7 +31,6 @@ class MotorController {
     array<float, 4> get_motor_speeds(float angle, float speed, float rotation);
 
     public:
-    void setup();
     void run_motors(float speed, float angle, float rotation);
     void run_raw(float tl_raw, float tr_raw, float bl_raw, float br_raw);
     void stop_motors();
